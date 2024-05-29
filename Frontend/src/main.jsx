28073308bbfbdd7fa1a90b4bc0f09c9222f54c24
ryansatj <134668057+ryansatj@users.noreply.github.com>
2024-05-30 02:35:44 +0700

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import App from './App.jsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Loginpage from './pages/Loginpage.jsx';
 import SignupPage from './pages/SignupPage.jsx';
 import AboutUs from './pages/AboutUsPage.jsx';
@@ -11,24 +11,44 @@ import AboutUsLog from './pages/AboutUsLogPage.jsx';
 import CreateReviewPage from './pages/CreateReviewPage.jsx';
 import EditPage from './pages/EditPage.jsx';
 
-const router = (
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="login" element={<Loginpage />} />
-      <Route path="signup" element={<SignupPage />} />
-      <Route path="about" element={<AboutUs />} />
-      <Route path="games" element={<MainPage />} />
-      <Route path="aboutus" element={<AboutUsLog />} />
-      <Route path="create" element={<CreateReviewPage />} />
-      <Route path="edit/:id" element={<EditPage />} />
-    </Routes>
-  </Router>
-);
 
-ReactDOM.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "login",
+    element: <Loginpage/>,
+  },
+  {
+    path: "signup",
+    element: <SignupPage/>
+  },
+  {
+    path: "about",
+    element: <AboutUs/>
+  },
+  {
+    path: "games",
+    element: <MainPage/>
+  },
+  {
+    path: "aboutus",
+    element: <AboutUsLog/>
+  },
+  {
+    path: "create",
+    element: <CreateReviewPage/>
+  },
+  {
+    path: "edit/:id",
+    element: <EditPage/>
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {router}
-  </React.StrictMode>,
-  document.getElementById("root")
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
